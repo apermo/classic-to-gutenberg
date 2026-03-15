4e44457c225fb1002878837286867e9b6753daca
Christoph Daum <christoph.daum@coding-pioneers.com>
2026-03-15 09:38:37 +0100

    feat(plugin): wire up converter registration
    Register all core converters and shortcode handlers
    in Plugin::create_factory(). Add create_content_converter()
    factory method. Hook CLI and admin init.

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace Apermo\ClassicToGutenberg;
 
 use Apermo\ClassicToGutenberg\Converter\BlockConverterFactory;
+use Apermo\ClassicToGutenberg\Converter\HeadingConverter;
 use Apermo\ClassicToGutenberg\Converter\HtmlBlockConverter;
+use Apermo\ClassicToGutenberg\Converter\ImageConverter;
+use Apermo\ClassicToGutenberg\Converter\ListConverter;
+use Apermo\ClassicToGutenberg\Converter\MoreConverter;
+use Apermo\ClassicToGutenberg\Converter\NextpageConverter;
+use Apermo\ClassicToGutenberg\Converter\ParagraphConverter;
+use Apermo\ClassicToGutenberg\Converter\PreformattedConverter;
+use Apermo\ClassicToGutenberg\Converter\QuoteConverter;
+use Apermo\ClassicToGutenberg\Converter\SeparatorConverter;
+use Apermo\ClassicToGutenberg\Converter\Shortcode\CaptionHandler;
+use Apermo\ClassicToGutenberg\Converter\Shortcode\GalleryHandler;
+use Apermo\ClassicToGutenberg\Converter\ShortcodeConverter;
+use Apermo\ClassicToGutenberg\Converter\TableConverter;
+use Apermo\ClassicToGutenberg\Parser\TopLevelSplitter;
 
 /**
  * Main plugin class.
@@ -18,7 +32,26 @@ class Plugin {
 	 * @return void
 	 */
 	public static function init(): void {
-		// Plugin initialization will be added as features are implemented.
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			add_action(
+				'cli_init',
+				static function (): void {
+					$converter = self::create_content_converter();
+					CLI\StatusCommand::register();
+					CLI\ConvertCommand::register( $converter );
+					CLI\RollbackCommand::register();
+				},
+			);
+		}
+
+		add_action(
+			'admin_init',
+			static function (): void {
+				$converter = self::create_content_converter();
+				new Admin\RowAction( $converter );
+				new Admin\AdminNotice();
+			},
+		);
 	}
 
 	/**
@@ -32,8 +65,39 @@ class Plugin {
 	public static function create_factory(): BlockConverterFactory {
 		$factory = new BlockConverterFactory( new HtmlBlockConverter() );
 
+		$factory->register( new ParagraphConverter() );
+		$factory->register( new HeadingConverter() );
+		$factory->register( new SeparatorConverter() );
+		$factory->register( new PreformattedConverter() );
+		$factory->register( new MoreConverter() );
+		$factory->register( new NextpageConverter() );
+		$factory->register( new ListConverter() );
+		$factory->register( new QuoteConverter() );
+		$factory->register( new TableConverter() );
+		$factory->register( new ImageConverter() );
+
+		/**
+		 * Filter to extend shortcode handler registry.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param \Apermo\ClassicToGutenberg\Converter\Shortcode\ShortcodeHandlerInterface[] $handlers Shortcode handlers.
+		 *
+		 * @return \Apermo\ClassicToGutenberg\Converter\Shortcode\ShortcodeHandlerInterface[]
+		 */
+		$shortcode_handlers = apply_filters(
+			'classic_to_gutenberg_shortcode_handlers',
+			[
+				new CaptionHandler(),
+				new GalleryHandler(),
+			],
+		);
+		$factory->register( new ShortcodeConverter( $shortcode_handlers ) );
+
 		/**
 		 * Allows registration of additional block converters.
+		 *
+		 * @since 0.1.0
 		 *
 		 * @param BlockConverterFactory $factory The converter factory.
 		 *
@@ -42,5 +106,18 @@ class Plugin {
 		apply_filters( 'classic_to_gutenberg_converters', $factory );
 
 		return $factory;
+	}
+
+	/**
+	 * Create a fully configured ContentConverter.
+	 *
+	 * @return ContentConverter
+	 */
+	public static function create_content_converter(): ContentConverter {
+		return new ContentConverter(
+			self::create_factory(),
+			new TopLevelSplitter(),
+			static fn( string $content ): string => wpautop( $content ),
+		);
 	}
 }
